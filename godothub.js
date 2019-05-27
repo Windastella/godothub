430c7c53bdfbbbfe0515bb5363723ec0174ce5d0
Nik Mirza <nik96mirza@gmail.com>
2019-05-28 07:07:01 +0800

    using db to manage clients

--- godothub.js
+++ godothub.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 var option = require('commander');
-
-var PORT = 5000;
-var HOST = 'localhost';
+var loki = require('lokijs');
+var db = new loki('memory.db');
 
 option
   .version('0.1.0')
@@ -16,15 +15,27 @@ option
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
+var users = db.addCollection('users');
 var clients = []
 
-function read_var(data){//Must be Json format
+function pushClient(client){
+  var index = clients.push(client) - 1;
+  users.insert({index, id:client.ID, channel:client.channel, address:client.address, port:client.port });
+}
+
+function removeClient(id){
+  var user = users.findOne({id});
+  clients.splice(user.index);
+  return user;
+}
+
+async function read_var(data){//Must be Json format
   data = data.toString(); //Change Buffer to string
   var str = data.substring(data.search("{"),data.lastIndexOf("}")+1)+"\n"; //retrieve the JSON substring
   return JSON.parse(str);
 }
 
-function send_var(data,port,address){//data as object
+async function send_var(data,port,address){//data as object
   data = JSON.stringify(data);
   var dataLength= Buffer.byteLength(data)
   // Round UP dataLength to nearest multiple of 4
@@ -36,7 +47,7 @@ function send_var(data,port,address){//data as object
   // 4 bytes for the type +
   // 4 bytes for the length of the string +
   // the length of the string, rounded up to the nearest multiple of 4
-  var response = new Buffer(4+4+dataLength)
+  var response = Buffer.alloc(4+4+dataLength)
   response.writeUIntLE(0x00000004,0,4) // Write the type, 4 is for strings
   response.writeUIntLE(Buffer.byteLength(data),4,4) // Write the length of the string (in bytes)
   response.write(data,8) // Write the actual string
@@ -55,19 +66,21 @@ server.on('listening', function () {
 });
 
 server.on('error',(err) =>{
-  console.log(err);
+  console.log("Server Error:",err);
 })
 
-server.on('message', function (data, client) {
-    data = read_var(data);
+server.on('message', async function (data, client) {
+  try{
+
+    data = await read_var(data);
     var dat = data.data;
 
     switch (dat.event) {
 
       case "connecting":
-        client.ID = "client:"+client.address+":"+client.port;
+        client.ID = client.address+":"+client.port;
         client.channel = data.channel;
-        clients.push(client);
+        pushClient(client);
 
         console.log(client.ID + " connected");
 
@@ -78,37 +91,31 @@ server.on('message', function (data, client) {
         break;
 
       case "disconnect":
-        for(var i=0;i<clients.length;i++){
-          if (clients[i].ID == data.ID){
+        console.log(data.ID + " Disconnected");
 
-            console.log(clients[i].ID + " Disconnected");
+        var user = removeClient(data.ID);
+        multicast({event:"left",msg:data.ID+" left the channel",ID:data.ID}, data.ID, user.channel);
 
-            multicast({event:"left",msg:clients[i].ID+" left the channel",ID:clients[i].ID}, clients[i].ID, clients[i].channel);
-            console.log(clients[i].ID + " left channel "+clients[i].channel);
+        console.log(data.ID + " left channel "+user.channel);
+        users.remove(user);
 
-            clients.splice(i);
-            break;
-          }
-        }
         break;
 
       case "channel":
-        for(var i=0;i<clients.length;i++){
-          if (clients[i].ID == data.ID){
+        var user = users.find({id:data.ID});
+        if (user.channel == data.channel)
+          return
 
-            if (clients[i].channel == data.channel)
-              return
+        multicast({event:"left",msg:user.ID+" left the channel",ID:user.ID}, user.ID, user.channel);
+        console.log(user.ID + " left channel "+user.channel);
 
-            multicast({event:"left",msg:clients[i].ID+" left the channel",ID:clients[i].ID}, clients[i].ID, clients[i].channel);
-            console.log(clients[i].ID + " left channel "+clients[i].channel);
+        user.channel = data.channel;
+        clients[user.index].channel = data.channel;
+        users.update(user);
 
-            clients[i].channel = data.channel;
+        multicast({event:"join",msg:client.ID+" join the channel",ID:clients[i].ID}, clients[i].ID, clients[i].channel);
+        console.log(clients[i].ID + " join channel "+clients[i].channel);
 
-            multicast({event:"join",msg:client.ID+" join the channel",ID:clients[i].ID}, clients[i].ID, clients[i].channel);
-            console.log(clients[i].ID + " join channel "+clients[i].channel);
-            break;
-          }
-        }
         break;
 
       case "broadcast":
@@ -116,7 +123,7 @@ server.on('message', function (data, client) {
         break;
 
       case "unicast":
-        unicast(dat, dat.ID);
+        unicast(dat, data.ID);
         break;
 
       case "multicast":
@@ -127,6 +134,10 @@ server.on('message', function (data, client) {
         unicast(dat, data.ID);//if default send back to sender
         break;
     }
+
+  }catch(err){
+    console.log("Server Error:",err);
+  }
 });
 
 // Send data to every channel
@@ -140,21 +151,18 @@ function broadcast(data, id){
 
 // Send data to a specified channel
 function multicast(data, id, channel){
-  for(var i=0;i<clients.length;i++){
-    if (clients[i].channel == channel && clients[i].ID != id){
-      send_var(data, clients[i].port, clients[i].address);
+  var res = users.find({channel});
+  for(var i=0;i<res.length;i++){
+    if ( res[i].id != id){
+      send_var(data, res[i].port, res[i].address);
     }
   }
 }
 
 // Send data to a specified client
 function unicast(data, id){
-  for(var i=0;i<clients.length;i++){
-    if (clients[i].ID == id){
-      send_var(data, clients[i].port,clients[i].address);
-      return
-    }
-  }
+  var res = users.findOne({id});
+  send_var(data, res.port, res.address);      
 }
 
 server.bind(option.port, option.host);
